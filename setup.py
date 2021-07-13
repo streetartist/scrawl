b214a3c5b0d831bbe34bf56005b85d2f577eaaa2
streetartist <60282562+streetartist@users.noreply.github.com>
2021-07-13 10:57:56 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from os import path
+DIR = path.dirname(path.abspath(__file__))
+INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
+with open(path.join(DIR, 'README.md')) as f:
+ README = f.read()
+setup(
+ name='scrawl_engine',
+ packages=['scrawl'],
+ description="Write game like Scratch",
+ long_description=README,
+ long_description_content_type='text/markdown',
+ install_requires=INSTALL_PACKAGES,
+ version='0.2.5',
+ url='http://github.com/streetartist/scrawl',
+ author='Streetartist',
+ author_email='15252635839@163.com',
+ keywords=['game', 'engine'],
+ tests_require=[
+ 'pytest',
+ 'pytest-cov',
+ 'pytest-sugar'
+ ],
+ python_requires='>=3'
+)
