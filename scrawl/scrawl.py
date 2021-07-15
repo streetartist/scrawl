6f953dba42ffa43d20b2040618bf998ca43854c3
streetartist <60282562+streetartist@users.noreply.github.com>
2021-07-15 00:50:03 +0000

    Update scrawl.py

--- scrawl/scrawl.py
+++ scrawl/scrawl.py
@@ -1,3 +1,6 @@
+import sys
+import pygame
+
 class Scene:
     pass
 
@@ -5,4 +8,18 @@ class Sprite:
     pass
 
 class Game:
-    pass
+    def run(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption(self.title)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            yield_list = {}
+            for sprite in sprites:
+                yield_return = sprite.main()
+                if yield_return in yield_list:
+                    yield_list[yield_return].append(sprite.main)
