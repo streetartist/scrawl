de92fc9686c070ad1dd6062f4104b7c74cf6c318
streetartist <60282562+streetartist@users.noreply.github.com>
2025-06-21 07:14:05 +0800

    Update __init__.py

--- scrawl/__init__.py
+++ scrawl/__init__.py
@@ -1,1 +1,1 @@
-from .engine import *
+from .engine import Game, Sprite, PhysicsSprite, Scene, as_main, as_clones, handle_broadcast, handle_edge_collision, handle_sprite_collision, on_key, Cat, ParticleSystem
