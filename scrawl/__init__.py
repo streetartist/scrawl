8a8ce34ae8904c45731a0ef5bee1e4dd93674307
streetartist <60282562+streetartist@users.noreply.github.com>
2025-06-22 09:10:16 +0800

    Update __init__.py

--- scrawl/__init__.py
+++ scrawl/__init__.py
@@ -1,6 +1,1 @@
 from .engine import Game, Sprite, PhysicsSprite, Scene, as_main, as_clones, handle_broadcast, handle_edge_collision, handle_sprite_collision, on_key, Cat, ParticleSystem
-
-import os
-
-# 获取当前包目录的绝对路径
-PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
