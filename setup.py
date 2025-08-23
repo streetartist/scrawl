268f122a0e3e8344c9aabddaf552987eb494d57a
Weber <207148178@qq.com>
2025-08-23 23:27:53 +0800

    优化setup.py的格式

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
 from setuptools import setup
 from os import path
+
 DIR = path.dirname(path.abspath(__file__))
 INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
+
 with open(path.join(DIR, 'README.md')) as f:
- README = f.read()
+    README = f.read()
+
 setup(
- name='scrawl_engine',
- packages=['scrawl'],
- description="Write game like Scratch",
- long_description=README,
- long_description_content_type='text/markdown',
- install_requires=INSTALL_PACKAGES,
- version='0.11.1',
- url='http://github.com/streetartist/scrawl',
- author='Streetartist',
- author_email='207148178@qq.com',
- keywords=['game', 'engine'],
- tests_require=[
- 'pytest',
- 'pytest-cov',
- 'pytest-sugar'
- ],
- python_requires='>=3',
- include_package_data=True
+    name='scrawl_engine',
+    packages=['scrawl'],
+    description="Write game like Scratch",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    install_requires=INSTALL_PACKAGES,
+    version='0.12.1',
+    url='http://github.com/streetartist/scrawl',
+    author='Streetartist',
+    author_email='207148178@qq.com',
+    keywords=['game', 'engine'],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pytest-sugar'
+        ],
+    python_requires='>=3',
+    include_package_data=True
 )
